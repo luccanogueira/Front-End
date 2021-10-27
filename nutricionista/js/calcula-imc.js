900e7163f4237268cc83efccb4b72f08aed0dd6b
Lucca Nogueira <72457899+luccanogueira@users.noreply.github.com>
2021-10-27 08:54:29 -0300

    Add files via upload

---
+++ nutricionista/js/calcula-imc.js
@@ -0,0 +1,59 @@
+var titulo = document.querySelector(".titulo")
+titulo.textContent = "Anderson Nutrição";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.clientHeight; i++){
+
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    if (!pesoEhValido) {
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (!alturaEhValida) {
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(pesoEhValido && alturaEhValida){
+        var imc = calculaImc(peso, altura);
+        tdImc.textContent = imc;
+    }
+}
+
+function calculaImc(peso, altura) {
+    var imc = 0;
+    imc = peso / (altura * altura);
+
+    return imc.toFixed(2);
+}
+
+function validaPeso(peso) {
+    if(peso >= 0 && peso <= 1000) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validaAltura(altura) {
+    if(altura >= 0 && altura <= 3.00) {
+        return true;
+    } else {
+        return false;
+    }
+}
